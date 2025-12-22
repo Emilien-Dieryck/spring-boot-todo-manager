b3286102ab2e7ae9bb206727033e972841207930
EmilienD <emilien.dieryck@student.vinci.be>
2025-12-22 17:44:16 +0100

    chore: init Spring Boot project

---
+++ src/main/java/com/example/todomanager/SpringBootTodoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.todomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTodoManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todomanager/SpringBootTodoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
